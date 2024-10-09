569c04661c102ae2342507addb1e10db63a22116
Stefano <stefano.stoduto@gmail.com>
2024-10-09 21:40:56 +0200

    remove unused telescope plugin folder
    this doesn't work as telescope plugin because it doesn't return a picker
    directly.
    Rather it wraps multiple pickers within a flow allowing to manage scopes
    through some kind of UI flow

--- lua/telescope/_extensions/neoscopes-telescope.lua
+++
@@ -1,6 +0,0 @@
-return require("telescope").register_extension {
-  setup = function(ext_config, telescope_config)
-    -- access extension config and user config
-  end,
-  exports = require("neoscopes-telescope.operations"),
-}
